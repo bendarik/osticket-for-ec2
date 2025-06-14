bf083c278d081dde8eada91ebce96b1138f3ed09
bendarik <bendarik@outlook.com>
2025-06-14 21:57:12 +0200

    added scripts and some bash

---
+++ scripts/docker-compose-down.sh
@@ -0,0 +1,2 @@
+sudo docker compose -f ./docker-compose-app.yaml down
+sudo docker compose -f ./docker-compose-proxy.yaml down
--- scripts/docker-compose-up.sh
+++ scripts/docker-compose-up.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
-# Start PROXY
-sudo docker compose -f ../docker-compose-proxy.yaml up -d
-if [ $? -ne 0 ]; then
-    echo "Failed to start Docker Compose PROXY."
-    exit 1
-else
-    echo "Docker Compose PROXY started successfully."
-fi
+# Start Docker Compose PROXY
+if sudo test -f ./certbot/conf/live/baobabka.com/fullchain.pem; then
+    
+    cp ./nginx/nginx-https.conf ./nginx/nginx.conf
+    sudo docker compose -f ./docker-compose-proxy.yaml up -d
 
-# Start APP
-sudo docker compose -f ../docker-compose-app.yaml up -d
+else
+
+    echo "[*] Starting nginx in HTTP-only mode..."
+    cp ./nginx/nginx-http.conf ./nginx/nginx.conf
+    sudo docker compose -f ./docker-compose-proxy.yaml up -d
+
+    echo "[*] Waiting for nginx to start..."
+    sleep 5
+
+    echo "[*] Replacing nginx config with HTTPS version..."
+    cp ./nginx/nginx-https.conf ./nginx/nginx.conf
+
+    echo "[*] Restarting nginx with HTTPS..."
+    sudo docker compose -f ./docker-compose-proxy.yaml stop nginx
+    sudo docker compose -f ./docker-compose-proxy.yaml start nginx
+
+    echo "[✔] Setup complete."
+
+fi
+echo "Docker Compose PROXY started successfully."
+
+
+# Start Docker Compose APP
+sudo docker compose -f ./docker-compose-app.yaml up -d
 if [ $? -ne 0 ]; then
     echo "Failed to start Docker Compose APP."
     exit 1
