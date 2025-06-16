c01f3ad9306ffdef2a940ee326abb21cab7e3d2d
bendarik <bendarik@outlook.com>
2025-06-16 18:39:52 +0200

    ps script for creating DNS records

--- scripts/create-DNS-records.ps1
+++ scripts/create-DNS-records.ps1
@@ -1,40 +1,89 @@
 
-# $CLOUDFLARE_EMAIL = "bendarik@outlook.com"
-# $CLOUDFLARE_API_KEY = "731edc7bb61ae713fba7c4e51f6c253dbccb7"
 $CLOUDFLARE_API_TOKEN = "SYJS4PsoO2JUflBpg-603BiLJJhYHkd_01beGyyS"
 $ZONE_ID = "3e0f330a288197b6bf89a7be4084df0e"
-# $ACCOUNT_ID = "4f054db98163f7b86c6b3222be0e736a"
-
-
-# $responce = Invoke-WebRequest -Uri "https://api.cloudflare.com/client/v4/user/tokens/verify" `
-#     -Headers @{ "Authorization" = "Bearer $token" }
-
-# if ($responce.StatusCode -eq 200) {
-#     Write-Host "Token is valid."
-#     Write-Host $responce.Content
-# }
-
 
 $uri = "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records";
 
-$params = @{
-    Uri     = $uri
-    # Method  = "GET"
-    Headers = @{ "Authorization" = "Bearer $CLOUDFLARE_API_TOKEN" }
-    # Body    = @{ "name" = "{exact = 'baobabka.com'}" }
-    Body    = @{ "name" = "baobabka.com" }
-}
+# Define the DNS record details
 
-$response = Invoke-WebRequest @params
+$DNS_RECORD_NAME = "baobabka.com"
+$PUBLIC_IP = "17.48.44.77"
 
-$rObj = ConvertFrom-Json $response.Content
-
-if ($rObj.success) {
-    Write-Host "DNS records retrieved successfully."
-    $rObj.result | ForEach-Object {
-        Write-Host "ID: $($_.id), Name: $($_.name), Type: $($_.type), Content: $($_.content)"
+$DNS_RECORDS = @(
+    @{
+        type    = "A"
+        name    = $DNS_RECORD_NAME
+        content = $PUBLIC_IP
+        ttl     = 3600
+        proxied = $false
+    },
+    @{
+        type    = "A"
+        name    = "www.$($DNS_RECORD_NAME)"
+        content = $PUBLIC_IP
+        ttl     = 3600
+        proxied = $false
     }
-} else {
+)
+
+#
+# 1. Check if the DNS record already exists
+$records_response = Invoke-WebRequest `
+    -Uri $uri `
+    -Method Get `
+    -Headers @{ "Authorization" = "Bearer $CLOUDFLARE_API_TOKEN" }
+$records_result = ConvertFrom-Json $records_response.Content
+if ($records_result.success) {
+    
+    #
+    # 2. Delete the existing DNS record.
+    $records_result.result | ForEach-Object {
+        if ($_.name -eq $DNS_RECORD_NAME -or $_.name -eq "www.$($DNS_RECORD_NAME)") {
+            
+            Write-Host "Found DNS record: $($_.name) (id $($_.id))"
+
+            $delete_response = Invoke-WebRequest `
+                -Uri "$($uri)/$($_.id)" `
+                -Method Delete `
+                -Headers @{ "Authorization" = "Bearer $CLOUDFLARE_API_TOKEN" }
+            $delete_result = ConvertFrom-Json $delete_response.Content
+            if ($delete_result.success) {
+                Write-Host "DNS record deleted successfully: $($_.name) (id $($_.id))"
+            }
+            else {
+                Write-Host "Failed to delete DNS record: $($_.name) (id $($_.id))"
+                Write-Host "Error: $($delete_result.errors | ConvertTo-Json)"
+            }
+        }
+    }
+
+    #
+    # 3. Create new DNS records.
+    foreach ($record in $DNS_RECORDS) {
+        $jsonBody = $record | ConvertTo-Json -Depth 3
+        try {
+            $create_response = Invoke-WebRequest `
+                -Uri $uri `
+                -Method Post `
+                -Headers @{ "Authorization" = "Bearer $CLOUDFLARE_API_TOKEN" } `
+                -ContentType "application/json" `
+                -Body $jsonBody
+            $create_result = ConvertFrom-Json $create_response.Content
+            if ($create_result.success) {
+                Write-Host "DNS record created successfully: $($record.name)"
+            }
+            else {
+                Write-Host "Failed to create DNS record: $($record.name)"
+                Write-Host "Error: $($delete_result.errors | ConvertTo-Json)"
+            }
+        }
+        catch {
+            Write-Host "Exception creating DNS record: $($record.name)"
+            Write-Host $_.Exception.Message
+        }
+    }
+}
+else {
     Write-Host "Failed to retrieve DNS records."
-    Write-Host "Error: $($rObj.errors | ConvertTo-Json)"
+    Write-Host "Error: $($records_result.errors | ConvertTo-Json)"
 }
