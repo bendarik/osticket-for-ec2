0eafc5efd0967ea8730ab43f74c197822b2512d0
bendarik <bendarik@outlook.com>
2025-06-20 13:21:30 +0200

    terraform - variables and ouotputs are added

--- main.tf
+++ main.tf
@@ -1,56 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 5"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-north-1"
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
-
-# Variables
-
-variable "cloudflare_api_token" {
-  type        = string
-  description = "Cloudflare API token with permissions to manage DNS records"
-  default     = "SYJS4PsoO2JUflBpg-603BiLJJhYHkd_01beGyyS"
-}
-
-variable "cloudflare_zone_id" {
-  type        = string
-  description = "Cloudflare zone ID for the domain"
-  default     = "3e0f330a288197b6bf89a7be4084df0e"
-}
-
-variable "domain_name" {
-  type        = string
-  description = "Domain name for the DNS record"
-  default     = "baobabka.com"
-}
-
-
+#
 # AWS Resources
 
 resource "aws_instance" "osticket" {
-  ami             = "ami-042b4708b1d05f512" # Canonical, Ubuntu, 24.04, amd64 noble image
-  instance_type   = "t3.micro"
+  ami             = var.aws_ami_id
+  instance_type   = var.aws_instance_type
   security_groups = [aws_security_group.osticker.name]
   key_name        = "osticket-key" # Ensure you have created this key pair in AWS
-#   user_data       = file("scripts/install-docker-engine.sh") # Path to your user data script for initial setup
+  # user_data       = file("scripts/install-docker-engine.sh") # Path to your user data script for initial setup
   tags = {
-    Name = "osticket-instance"
+    Name = var.aws_instance_name
   }
 }
 
@@ -98,7 +56,7 @@ resource "aws_security_group_rule" "allow_osticket_all_outbound" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-
+#
 # Cloudflare DNS Records
 
 resource "cloudflare_dns_record" "dns_record_www" {
